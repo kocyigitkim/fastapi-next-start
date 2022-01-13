423885581078662d1f77717844c6ed5c876044be
Muhammed Koçyiğit <muhammetkocyigit0@gmail.com>
2022-01-13 21:36:41 +0300

    build deleted.

--- build/routers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fastapi_next_1 = require("fastapi-next");
-async function default_1(ctx) {
-    var response = new fastapi_next_1.ApiResponse();
-    response.setSuccess("Hello World");
-    return response;
-}
-exports.default = default_1;
